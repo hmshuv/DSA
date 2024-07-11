abf2d8adef70203a097269dc6af1e88039485d5d
Himanshu Gupta <himanshugupta@Himanshus-Air.lan>
2024-07-12 00:18:54 +0530

    commit

--- star_pattern/star_patterns.cpp
+++ star_pattern/star_patterns.cpp
@@ -18,6 +18,29 @@ void pattern2(int n){
         cout << "\n";
     }
 }
+void pattern3(int n){
+    for(int i = 0 ; i < n ; i++){
+        for(int j = 0 ; j <= i; j++){
+            cout << j + 1;
+        }
+        cout << "\n";
+    }
+}
+void pattern4(int n){
+    for(int i = 0 ; i < n ; i++){
+        for(int j = 0 ; j <= i; j++){
+            cout << i + 1;
+        }
+        cout << "\n";
+    }
+}
+void pattern5(int n){
+    for(int i = 0 ; i < n ; i++){
+    for(int j = 0 ; j < n - i; j++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
 }
 
 int main(){
